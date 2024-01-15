8686d7e073f2f594efab21cd46c1fcf0e50f52f1
Mahanyasa Keesara <72213008+Mahanyasa@users.noreply.github.com>
2024-01-15 14:16:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function toggleProject() {
+  var x = document.getElementById('hdn-dv');
+  var b = document.getElementById('btn-prj');
+
+  if (x.style.display !== 'block') {
+    x.style.display = 'block';
+    b.childNodes[0].nodeValue = "Less";
+
+  } else {
+    x.style.display = 'none';
+    b.childNodes[0].nodeValue = "More";
+  }
+
+}
